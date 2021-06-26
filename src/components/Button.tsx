6090e3164e635bc593159cc63ef54be010df3385
Lucas Otaviani Bergamo de Oliveira <47718917+LucasOtaviani@users.noreply.github.com>
2021-06-26 10:59:24 -0300

    Revert "Aula 4 (#2)" (#3)
    This reverts commit 082af44612b205f79d1a2681b9e66d190f41b47e.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,12 +2,10 @@ import { ButtonHTMLAttributes } from 'react'
 
 import '../styles/button.scss'
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & { 
-    isOutlined?: boolean
-}
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 
-export function Button({ isOutlined = false, ...props }: ButtonProps) {
+export function Button(props: ButtonProps) {
     return (
-        <button className={`button ${isOutlined ? 'outlined' : ''}`} { ...props }></button>
+        <button className="button" { ...props }></button>
     )
 }
